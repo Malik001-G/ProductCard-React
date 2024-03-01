26937e7ed90ec8c26b702c2466996b15126af3e7
Malik001-G <adisamalikng0@gmail.com>
2024-03-01 16:29:14 +0100

    Product card using React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const firstShoe = {
+//   description:
+//     "Clean and supreme, the AJ3 returns. Pairing a smooth Ivory base with Grey Mist suede overlays and translucent accents on the heel tab and sole, this crafted edition ups the texture. It has dual branding on the heel and double Jumpman logos on the tongue—because twice the Jumpman is twice as nice.",
+//   name: 'Air Jordan 3 Retro Craft "Ivory"',
+//   img: "./images/shoe-1.png",
+// };
+// const secondShoe = {
+//
+// };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function CardList() {
+//   return (
+//     <section className="cardList">
+//       <Card
+//         img={firstShoe.img}
+//         name={firstShoe.name}
+//         description={firstShoe.description}
+//       />
+//       <Card
+//         img={secondShoe.img}
+//         name={secondShoe.name}
+//         description={secondShoe.description}
+//       />
+//     </section>
+//   );
+// }
+
+// const Card = (props) => {
+//   console.log(props);
+//   <section className="product">
+//     <img src={props.img} alt={props.name} />
+//     <h2 className="product__title">{props.name}</h2>
+//     <p>{props.description}</p>
+//     {console.log(props)}
+//   </section>;
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<CardList />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const shoes = [
+  {
+    description:
+      'The Air Jordan 3 Retro Craft "Ivory" combines timeless style with modern craftsmanship. Featuring a sleek Ivory base, Grey Mist suede overlays, and translucent accents, it offers a refined aesthetic.',
+    name: 'Air Jordan 3 Retro Craft "Ivory"',
+    img: "./images/first.png",
+    price: "$250",
+    id: 1,
+  },
+  {
+    description:
+      'Air Jordan 13 Retro "Blue Grey" combines timeless style with modern craftsmanship. Featuring a sleek Ivory base, Grey Mist suede overlays, and translucent accents, it offers a refined aesthetic.',
+    name: 'Air Jordan 13 Retro "Blue Grey"',
+    img: "./images/first.png",
+    price: "$200",
+    id: 2,
+  },
+  {
+    description:
+      "Nike Dunk Low Retro Premium combines timeless style with modern craftsmanship. Featuring a sleek Ivory base, Grey Mist suede overlays, and translucent accents, it offers a refined aesthetic.",
+    name: "Nike Dunk Low Retro Premium",
+    img: "./images/first.png",
+    price: "$125",
+    id: 3,
+  },
+];
+
+function CardList() {
+  return (
+    <section className="cardList">
+      {shoes.map((card) => {
+        console.log(card);
+        return <Card {...card} key={card.id} />;
+      })}
+    </section>
+  );
+}
+
+const Card = (props) => {
+  const { img, name, description, price } = props;
+  return (
+    <section className="product">
+      <img className="product__image" src={img} alt={name} />
+      <h2 className="product__title">{name}</h2>
+      <p>{description}</p>
+      <span className="product__price ">{price}</span>
+      <button className="product__btn btn">BUY NOW</button>
+    </section>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<CardList />);
